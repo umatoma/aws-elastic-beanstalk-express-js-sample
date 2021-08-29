6b7e945c51e8e338a853b99399f0db5f0045130e
umatoma <umatomakun@gmail.com>
2021-08-29 22:06:55 +0900

    update message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('HELLO HELLO!'));
+app.get('/', (req, res) => res.send('HELLO HELLO HELLO!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
